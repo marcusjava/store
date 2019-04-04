121040b6b7383c5e008dc6685e9abec7edd6424d
marcusjava <marcusbuba@hotmail.com>
2019-04-04 18:47:03 -0300

    paypal configurado e env

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import PaypalButton from './PaypalButton';
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
 	const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
 	return (
 		<Fragment>
@@ -35,6 +36,7 @@ export default function CartTotals({ value }) {
 								<strong>R$ {cartTotal}</strong>
 							</span>
 						</h5>
+						<PaypalButton total={cartTotal} clearCart={clearCart} history={history} />
 					</div>
 				</div>
 			</div>
